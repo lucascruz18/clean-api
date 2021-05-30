d50a5100c06a6d02f86096cec70a71e364f6e149
Lucas <lucascruzati@gmail.com>
2021-05-30 16:29:29 -0300

    fix: db-add-account-factory

--- src/main/routes/user-routes.test.ts
+++ src/main/routes/user-routes.test.ts
@@ -27,14 +27,4 @@ describe('SignUp Routes', () => {
       })
       .expect(200)
   })
-
-  test('Should return 401 on login', async () => {
-    await request(app)
-      .post('/api/login')
-      .send({
-        email: 'lucascruz@gmail.com',
-        password: '123'
-      })
-      .expect(401)
-  })
 })
