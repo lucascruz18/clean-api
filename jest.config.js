491391e3be0a8f0894c7c4136978faf9b2e4645b
Lucas <lucascruzati@gmail.com>
2021-08-25 22:14:28 -0300

    chore: add coverage provider

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
     '!<rootDir>/src/main/**'
   ],
-  // The directory where Jest should output its coverage files
   coverageDirectory: 'coverage',
-  // The test environment that will be used for testing
+  coverageProvider: 'babel',
   testEnvironment: 'node',
-  //
   preset: '@shelf/jest-mongodb',
-  // A map from regular expressions to paths to transformers
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
